b6ccb24e14ac79092b9a0cad84232c9d73b6a92c
Lucas Turci <lucas.turci@gmail.com>
2022-01-02 12:07:14 -0300

    Fix code to act as a webhook, not a server poller

--- function.go
+++ function.go
@@ -9,6 +9,7 @@ import (
 )
 
 func Handle(w http.ResponseWriter, r *http.Request) {
+
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
 	if err != nil {
 		log.Printf("Error with telegram authentication: %s", err.Error())
@@ -18,43 +19,36 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 
 	bot.Debug = true
 
-	// Create a new UpdateConfig struct with an offset of 0. Offsets are used
-	// to make sure Telegram knows we've handled previous values and we don't
-	// need them repeated.
-	updateConfig := tgbotapi.NewUpdate(0)
+	update, err := bot.HandleUpdate(r)
+	if err != nil {
+		log.Printf("Error when parsing update: %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-	// Start polling Telegram for updates.
-	updates := bot.GetUpdatesChan(updateConfig)
+	if update.Message == nil {
+		return
+	}
 
-	// Let's go through each update that we're getting from Telegram.
-	for update := range updates {
-		// Telegram can send many types of updates depending on what your Bot
-		// is up to. We only want to look at messages for now, so we can
-		// discard any other updates.
-		if update.Message == nil {
-			continue
-		}
+	log.Printf("Replying to message ID (%v) with body (%v)", update.Message.MessageID, update.Message.Text)
 
-		log.Printf("Replying to message ID (%v) with body (%v)", update.Message.MessageID, update.Message.Text)
+	// Now that we know we've gotten a new message, we can construct a
+	// reply! We'll take the Chat ID and Text from the incoming message
+	// and use it to create a new message.
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+	// We'll also say that this message is a reply to the previous message.
+	// For any other specifications than Chat ID or Text, you'll need to
+	// set fields on the `MessageConfig`.
+	msg.ReplyToMessageID = update.Message.MessageID
 
-		// Now that we know we've gotten a new message, we can construct a
-		// reply! We'll take the Chat ID and Text from the incoming message
-		// and use it to create a new message.
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		// We'll also say that this message is a reply to the previous message.
-		// For any other specifications than Chat ID or Text, you'll need to
-		// set fields on the `MessageConfig`.
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		// Okay, we're sending our message off! We don't care about the message
-		// we just sent, so we'll discard it.
-		if _, err := bot.Send(msg); err != nil {
-			// Note that panics are a bad way to handle errors. Telegram can
-			// have service outages or network errors, you should retry sending
-			// messages or more gracefully handle failures.
-			log.Printf("Error with sending message reply to message id (%v): %s", update.Message.MessageID, err.Error())
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
+	// Okay, we're sending our message off! We don't care about the message
+	// we just sent, so we'll discard it.
+	if _, err := bot.Send(msg); err != nil {
+		// Note that panics are a bad way to handle errors. Telegram can
+		// have service outages or network errors, you should retry sending
+		// messages or more gracefully handle failures.
+		log.Printf("Error with sending message reply to message id (%v): %s", update.Message.MessageID, err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 }
