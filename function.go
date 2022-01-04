04f99469fb5ea9a33263e56db6e59f2b43d31900
Lucas Turci <lucas.turci@gmail.com>
2022-01-04 17:15:24 -0300

    Code refactoring

--- function.go
+++ function.go
@@ -8,6 +8,33 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+func HandleUpdate(bot *tgbotapi.BotAPI, update *tgbotapi.Update) error {
+	if update.Message == nil {
+		return nil
+	}
+
+	log.Printf("Replying to message ID (%v) with body (%v)", update.Message.MessageID, update.Message.Text)
+
+	// Now that we know we've gotten a new message, we can construct a
+	// reply! We'll take the Chat ID and Text from the incoming message
+	// and use it to create a new message.
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+
+	// We'll also say that this message is a reply to the previous message.
+	// For any other specifications than Chat ID or Text, you'll need to
+	// set fields on the `MessageConfig`.
+	msg.ReplyToMessageID = update.Message.MessageID
+
+	// Okay, we're sending our message off! We don't care about the message
+	// we just sent, so we'll discard it.
+	if _, err := bot.Send(msg); err != nil {
+		log.Printf("Error when sending message reply to message id (%v): %s", update.Message.MessageID, err.Error())
+		return err
+	}
+
+	return nil
+}
+
 func Handle(w http.ResponseWriter, r *http.Request) {
 
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
@@ -26,29 +53,9 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if update.Message == nil {
-		return
-	}
-
-	log.Printf("Replying to message ID (%v) with body (%v)", update.Message.MessageID, update.Message.Text)
-
-	// Now that we know we've gotten a new message, we can construct a
-	// reply! We'll take the Chat ID and Text from the incoming message
-	// and use it to create a new message.
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-	// We'll also say that this message is a reply to the previous message.
-	// For any other specifications than Chat ID or Text, you'll need to
-	// set fields on the `MessageConfig`.
-	msg.ReplyToMessageID = update.Message.MessageID
-
-	// Okay, we're sending our message off! We don't care about the message
-	// we just sent, so we'll discard it.
-	if _, err := bot.Send(msg); err != nil {
-		// Note that panics are a bad way to handle errors. Telegram can
-		// have service outages or network errors, you should retry sending
-		// messages or more gracefully handle failures.
-		log.Printf("Error with sending message reply to message id (%v): %s", update.Message.MessageID, err.Error())
+	if err := HandleUpdate(bot, update); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		return
+		log.Printf("Error when handling update")
 	}
+
 }
